b58a8221b66425a80cecdf369e68fc4b5ba48515
jayval254 <valariekimuge1@gmail.com>
2023-05-14 03:48:33 -0700

    AirBnB Clone

--- valarie.py
+++ valarie.py
@@ -1,6 +1,16 @@
-#!/usr/bin/python3
-def print_numbers(x, y):
-    print("{:d} and {:d}".format(x, y))
+def print_letters():
+    """Print the letters 'x' and 'y'."""
+    letter_x = "x"
+    letter_y = "y"
+
+    print(letter_x)
+    print(letter_y)
 
 
-print_numbers(20, 23)
+def main():
+    """Main function."""
+    print_letters()
+
+
+if __name__ == "__main__":
+    main()

1ef9cd03f3e7791ad949bd18f90fad6627270a0a
mukhnaya <mosesmukhanya@gmail.com>
2023-05-15 01:28:19 +0000

    AirBnB updates

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""__init__ file for FileStorage
-"""
+'''__init__ file for FileStorage'''
 from models.engine.file_storage import FileStorage
+
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import models
 
 
-class BaseModel():
+class BaseModel:
     """
         Base model class
     """
